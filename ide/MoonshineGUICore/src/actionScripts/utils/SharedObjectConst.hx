ed6ade7be249700187d1b9d5b691099301034d2f
Tamas Sopronyi <tommysoprano@gmail.com>
2022-06-15 17:49:05 +0200

    Backend conversion update

---
+++ ide/MoonshineGUICore/src/actionScripts/utils/SharedObjectConst.hx
@@ -0,0 +1,10 @@
+package actionScripts.utils;
+
+class SharedObjectConst {
+	public static final MOONSHINE_IDE_LOCAL:String = "moonshine-ide-local";
+	public static final CONSOLE_HISTORY:String = "consoleHistory";
+	public static final REPOSITORY_HISTORY:String = "repositoriesHistory";
+	public static final MOONSHINE_IDE_PROJECT:String = "moonshine-ide-project";
+	public static final MOONSHINE_IDE_WORKSPACE:String = "moonshine-ide-workspace";
+	public static final SHARED_UPDATE_CHECKER:String = "sharedObjectTesterAndUpdater";
+}
