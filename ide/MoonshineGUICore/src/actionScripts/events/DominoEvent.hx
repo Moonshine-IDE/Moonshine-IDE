6d8261a4ef85688fb1309ddec24f9f47a8c82465
Tamas Sopronyi <tommysoprano@gmail.com>
2022-07-18 10:43:31 +0200

    Updated DominoEvent with the latest changes

--- ide/MoonshineGUICore/src/actionScripts/events/DominoEvent.hx
+++ ide/MoonshineGUICore/src/actionScripts/events/DominoEvent.hx
@@ -4,6 +4,7 @@ import openfl.events.Event;
 
 class DominoEvent extends Event {
 	public static final NDS_KILL:String = "eventNDSKill";
+	public static final EVENT_CONVERT_DOMINO_DATABASE:String = "eventConvertDominoDatabase";
 
 	public function new(type:String) {
 		super(type, false, false);
