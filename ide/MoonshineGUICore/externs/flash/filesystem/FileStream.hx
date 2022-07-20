1d91455c17f275efe8e41c33048a17401b964c0a
Tamas Sopronyi <tommysoprano@gmail.com>
2022-07-20 15:47:22 +0200

    Updated extern

--- ide/MoonshineGUICore/externs/flash/filesystem/FileStream.hx
+++ ide/MoonshineGUICore/externs/flash/filesystem/FileStream.hx
@@ -1,11 +1,14 @@
 package flash.filesystem;
 
+import openfl.utils.ByteArray;
+
 extern class FileStream {
 
     public function new();
     public function close():Void;
     public function open(file:File, fileMode:String):Void;
     public function readUTFBytes(length:Int):String;
+    public function writeBytes(bytes:ByteArray, offset:Int = 0, length:Int = 0):Void;
     public function writeUTFBytes(value:String):Void;
 
 }
